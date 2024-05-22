91cefd5cd656e42702a41b282c63c71d47a9a3bb
sgcloud3000 <sgcloud3000@gmail.com>
2024-05-22 12:20:05 -0500

    Added shopping list func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+
+const listItems = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+);
+
+
+function MyForm(){
+
+  return (
+    <form action="" method="get" class="App-form">
+      <div class="form-example">
+      <label for="name">Enter your name: </label>
+      <input type="text" name="name" id="name" required />
+      </div>
+      <div class="form-example">
+        <label for="email">Enter your email: </label>
+        <input type="email" name="email" id="email" required />
+      </div>
+      <div class="form-example">
+        <MyButton/>
+      </div>
+    </form>
+  );
+}
+function MyButton(){
+
+  return(
+    /*
+    <button>
+      I'm a button
+    </button>
+    */
+      <input type="submit" value="Subscribe!" />
+  );
+}
+function MyShoppingList(){
+
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+  
+  const listItems = products.map(product =>
+    <li 
+      key={product.id}
+      style={{color:product.isFruit ? 'magenta' : 'darkgreen'}}
+      >
+      {product.title}
+    </li>
+  );
+  return(
+    /*
+    <button>
+      I'm a button
+    </button>
+    */
+      {listItems}
+  );
+}
+export default function MyApp(){
+  return (
+    <div className="App">
+      <h1 className="App-heading">Welcome to my app</h1>
+      <MyForm/>
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
@@ -22,4 +100,4 @@ function App() {
   );
 }
 
-export default App;
+//export default App;
